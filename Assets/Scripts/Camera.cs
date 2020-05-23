639df1deac7f3a42797b487d327974f89edabc04
Dima Konovko <dima.konovko@mail.ru>
2020-05-23 23:15:37 +0300

    choto daze rabotaet

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform target;
+    public float height = 5f;
+    public float distance = 10f;
+
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y + height, target.position.z) - target.forward * distance;
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+            float translation = Input.GetAxis("Vertical") * speed;
+            float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+            translation *= Time.deltaTime;
+            rotation *= Time.deltaTime;
+            transform.Translate(0, 0, translation);
+            transform.Rotate(0, rotation, 0);
+    }
+}
